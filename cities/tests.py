34d7e393181cd3e186a99277c3b33edb02781a20
Israel <israelliasbo@gmail.com>
2022-03-29 22:40:19 -0400

    first test failed

--- cities/tests.py
+++ cities/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
 
-# Create your tests here.
+from cities.models import City
+
+
+class AnimalTestCase(TestCase):
+	def setUp(self):
+		City.objects.create(name='Bogotá', admin_name='Bogotá')
+		City.objects.create(name='Medellín', admin_name='Antioquia')
+
+	def test_animals_can_speak(self):
+		"""Animals that can speak are correctly identified"""
+		bogota = City.objects.get(name='Bogotá')
+		medellin = City.objects.get(name='Medellín')
+		self.assertEqual(bogota.name, 'Bogotá')
+		self.assertEqual(medellin.admin_name, 'Antioquia')
